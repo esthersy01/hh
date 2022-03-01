b277a0aaf4d8d245e0100d2a9f87f0bae915f587
esthersy01 <100187190+esthersy01@users.noreply.github.com>
2022-03-01 15:44:47 +0800

    Add files via upload

--- Untitled1.c
+++ Untitled1.c
@@ -2,13 +2,22 @@
 
 int main() {
 
-    int a;
-    char b;
-    short c;
-    long d;
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int d = 4;
+    int e = 5;
 
-    printf("%d\n", sizeof(a));
-    printf("%d\n", sizeof(b));
-    printf("%d\n", sizeof(c));
-    printf("%d\n", sizeof(d));
+    printf("請輸入五個數字，以空白隔開。\n");
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    //add scanf code to get data from input
+    //and feed those input into a, b, c, d, e, respectively.
+    //only ONE line code is you need
+    //please refer page 18 of slides
+
+    printf("a=%d\n", a);
+    printf("b=%d\n", b);
+    printf("c=%d\n", c);
+    printf("d=%d\n", d);
+    printf("e=%d\n", e);
 }
