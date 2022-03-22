beb600bf4a0bd00ac49102e5b98f50d70fa163b3
esthersy01 <100187190+esthersy01@users.noreply.github.com>
2022-03-22 14:26:51 +0800

    5888

--- print_variable.c
+++ print_variable.c
@@ -5,9 +5,8 @@ int main() {
     int b = 87;
     for (int j = 5; j < 23; j++) {
         
-
-            b=87*j;
-        printf("%d\n",b);
+        
+        printf("j=%d, b=%d\n",j,b*j);
         
     }
 }
